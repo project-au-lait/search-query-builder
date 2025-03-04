6e4adb69d28729808f13d5ea5ed9a7e1245be7bc
Yuichi Kuwahara <yuichi.kuwahara.0@gmail.com>
2025-03-05 08:28:56 +0900

    fix: modify for extention

--- sqb-core/src/main/java/dev/aulait/sqb/SearchResult.java
+++ sqb-core/src/main/java/dev/aulait/sqb/SearchResult.java
@@ -2,11 +2,15 @@ package dev.aulait.sqb;
 
 import java.util.ArrayList;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Value;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Value
+@Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SearchResult<T> {
 
   @Builder.Default private List<T> list = new ArrayList<>();
